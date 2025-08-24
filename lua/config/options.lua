12cf08e49619403234ee93c9769ad74f247582d4
dang <dang.nguyen14082002@gmail.com>
2025-08-24 23:27:12 +0700

    Disable mouse

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,7 @@
+vim.opt.expandtab = true
+vim.opt.mouse = ""
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.statuscolumn = '%s %l %r'
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
